f3aabce1c5db230ffb2989e6804873704b7b479b
Timo Baumann <baumann@informatik.uni-hamburg.de>
2017-04-20 16:07:58 -0400

    test that nonsense features make us fail

--- test/edu/cmu/inmind/openfacequalifier/input/TestFeaturesFactory.java
+++ test/edu/cmu/inmind/openfacequalifier/input/TestFeaturesFactory.java
@@ -11,7 +11,7 @@ import edu.cmu.inmind.openfacequalifier.FeatureType;
 public class TestFeaturesFactory {
 
 	@Test
-	public void test() {
+	public void testSimple() {
 		FeaturesFactory featfac = FeaturesFactory.newFeaturesFactoryFromLine("frame, timestamp, confidence");
 		Map<FeatureType,Float> f = featfac.newFromLine("1, 2, 3");
 		assertEquals(f.get(FeatureType.frame), 1, 0.0001);
@@ -21,5 +21,10 @@ public class TestFeaturesFactory {
 		assertEquals(f.get(FeatureType.frame), 1, 0.0001);
 		assertEquals(f.get(FeatureType.timestamp), 2, 0.0001);
 		assertEquals(f.get(FeatureType.confidence), 3, 0.0001);
+	}
+	
+	@Test(expected=java.lang.IllegalArgumentException.class)
+	public void testFail() {
+		FeaturesFactory.newFeaturesFactoryFromLine("INEXISTENT_FEATURE_IN_OPENFACE, timestamp, confidence");	
 	}
 }
