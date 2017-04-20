a0bde147f87e2a1db95858c8c4a7da36dbd2ba9a
Timo Baumann <baumann@informatik.uni-hamburg.de>
2017-04-20 15:47:19 -0400

    feature factory -- i wonder if it works...

--- src/edu/cmu/inmind/openfacequalifier/input/FeaturesFactory.java
+++ src/edu/cmu/inmind/openfacequalifier/input/FeaturesFactory.java
@@ -1,15 +1,39 @@
 package edu.cmu.inmind.openfacequalifier.input;
 
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.Iterator;
+import java.util.List;
+
+import edu.cmu.inmind.openfacequalifier.FeatureType;
 import edu.cmu.inmind.openfacequalifier.Features;
 
 public class FeaturesFactory {
+
+	final List<FeatureType> orderingOfFeatures;
+	
+	private FeaturesFactory(List<FeatureType> orderingOfFeatures) {
+		this.orderingOfFeatures = orderingOfFeatures;
+	}
 	
 	public static FeaturesFactory newFeaturesFactoryFromLine(String line) {
-		return null;
+		List<FeatureType> orderingOfFeatures = new ArrayList<>();
+		for (String tok : line.trim().split(", ")) {
+			FeatureType ft = FeatureType.valueOf(tok);
+			assert ft != null;
+			orderingOfFeatures.add(ft);
+		}
+		return new FeaturesFactory(orderingOfFeatures);
 	}
 	
 	public Features newFromLine(String line) {
-		return null;
+		EnumMap<FeatureType, Float> f = new EnumMap<>(FeatureType.class);
+		Iterator<FeatureType> featIt = orderingOfFeatures.iterator();
+		for (String tok : line.trim().split(", ")) {
+			float value = Float.parseFloat(tok);
+			f.put(featIt.next(), value);
+		}
+		return (Features) f;
 	}
 
 }
