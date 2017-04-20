80b1e3af908ad592acaef33b98ed84eb0ef50519
Timo Baumann <baumann@informatik.uni-hamburg.de>
2017-04-20 15:16:04 -0400

    dummy code to get rid of compile warnings

--- src/edu/cmu/inmind/openfacequalifier/eventDetector/RuleBasedEventDetector.java
+++ src/edu/cmu/inmind/openfacequalifier/eventDetector/RuleBasedEventDetector.java
@@ -1,11 +1,19 @@
 package edu.cmu.inmind.openfacequalifier.eventDetector;
 
+import edu.cmu.inmind.openfacequalifier.Event;
+import edu.cmu.inmind.openfacequalifier.Features;
 import edu.cmu.inmind.openfacequalifier.input.OpenFaceInput;
 
 public class RuleBasedEventDetector extends EventDetector {
 
 	public RuleBasedEventDetector(OpenFaceInput ofi) {
-		
+		super(ofi);
+	}
+
+	@Override
+	Event consumeFrame(Features f) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/edu/cmu/inmind/openfacequalifier/output/VHTOutput.java
+++ src/edu/cmu/inmind/openfacequalifier/output/VHTOutput.java
@@ -1,5 +1,12 @@
 package edu.cmu.inmind.openfacequalifier.output;
 
+import edu.cmu.inmind.openfacequalifier.Event;
+
 public class VHTOutput implements EventOutput {
+
+	@Override
+	public void nextEvent(Event e) {
+		System.err.println("not sending to VHT");
+	}
 
 }
