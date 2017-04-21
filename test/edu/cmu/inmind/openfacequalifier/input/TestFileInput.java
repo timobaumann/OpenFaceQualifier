567a947e9873e5b48db2f1670a089363e926f9c7
Timo Baumann <baumann@informatik.uni-hamburg.de>
2017-04-21 10:16:16 -0400

    test for file-based processing

---
+++ test/edu/cmu/inmind/openfacequalifier/input/TestFileInput.java
@@ -0,0 +1,28 @@
+package edu.cmu.inmind.openfacequalifier.input;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.junit.Test;
+
+import edu.cmu.inmind.openfacequalifier.FeatureType;
+import edu.cmu.inmind.openfacequalifier.output.CSVOutput;
+
+public class TestFileInput {
+
+	@Test
+	public void test() throws IOException, InterruptedException {
+		OpenFaceInput ofi = new FileInput(this.getClass().getResource("timo1.openface").toString().replaceFirst("^file:", ""));
+		//OpenFaceInput ofi = new ProcessInput("rtsp://34.203.204.136:8554/live/myStreamcdf20e73589e8763");
+		CSVOutput out = new CSVOutput(System.err);
+		while (ofi.hasMoreFrames()) {
+			Map<FeatureType,Float> f = ofi.getFeaturesForNextFrame();
+			if (f != null)
+				out.consumeFrame(f);
+			Thread.sleep(5);
+		}
+	}
+
+}
