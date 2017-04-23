05c886a3e33f1a4808f427843ca6cc139a998676
Timo Baumann <baumann@informatik.uni-hamburg.de>
2017-04-22 21:37:18 -0400

    have a timeout (0.5 seconds) so that we don't wait forever when transmission has ended.

--- src/edu/cmu/inmind/openfacequalifier/input/FileInput.java
+++ src/edu/cmu/inmind/openfacequalifier/input/FileInput.java
@@ -2,6 +2,8 @@ package edu.cmu.inmind.openfacequalifier.input;
 
 import java.io.File;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.SynchronousQueue;
 
 import org.apache.commons.io.input.Tailer;
@@ -11,14 +13,17 @@ import edu.cmu.inmind.openfacequalifier.FeatureType;
 
 public class FileInput extends TailerListenerAdapter implements OpenFaceInput {
 
-	SynchronousQueue<String> lines;
 	private FeaturesFactory featFac;
+
+	SynchronousQueue<String> lines;
 	Tailer tailer;
 	/** stop once EOF is reached (i.e., do not use wait heuristics) */
 	boolean singleFileMode = false;
 
-	/** implicit constructor to be used by subclasses */
+	/** implicit constructor to be used by subclasses; make sure to call openFile() when you subclass! */
 	FileInput() {}
+	
+	/* * setup of the file-reading * */
 	
 	public FileInput(String filename) {
 		openFile(filename);
@@ -26,49 +31,43 @@ public class FileInput extends TailerListenerAdapter implements OpenFaceInput {
 	}
 	
 	void openFile(String filename) {
+		lines = new SynchronousQueue<>();
 		tailer = new Tailer(new File(filename), this, 20); // 20 milliseconds of delay between read attempts
 		new Thread(tailer).start();
-		lines = new SynchronousQueue<>();
 	}
 	
+	/* * implementation of the OpenFaceInput interface * */
+
 	@Override
 	public Map<FeatureType,Float> getFeaturesForNextFrame() {
 		Map<FeatureType,Float> f = null;
 		try {
+			waitingThread = Thread.currentThread();
 			String line = lines.take();
+			waitingThread = null;
 			if (line != null) {
 				assert !line.startsWith("frame") : "I was not expecting this line in the middle";
 				f = featFac.newFromLine(line);
 			}
 		} catch (InterruptedException e) {
-			e.printStackTrace();
-			destroyForcibly();
+			if (hasMoreFrames()) {
+				e.printStackTrace();
+				destroyForcibly();
+			} // else: we're already shutting down and this causes the interruption 
 		}
 		return f;
 	}
-	
-	public void destroyForcibly() {
-		tailer.stop();
-		tailer = null;
-	}
-	
+
 	@Override
 	public boolean hasMoreFrames() {
 		return tailer != null;
 	}
-	
-	@Override
-	public void endOfFileReached() {
-		System.err.println("waiting for further input");
-		if (singleFileMode) {
-			destroyForcibly();
-		} else {
-			// TODO: implement some teardown timeout
-		}
-	}
-	
+
+	/* * overriding the relevant parts of the TailListenerAdapter * */
+
 	@Override
 	public void handle(String line) {
+		cancelTimeout();
 		if (featFac == null)
 			featFac = FeaturesFactory.newFeaturesFactoryFromLine(line);
 		else 
@@ -79,4 +78,46 @@ public class FileInput extends TailerListenerAdapter implements OpenFaceInput {
 			}
 	}
 
+	@Override
+	public void endOfFileReached() {
+		//System.err.println("waiting for further input");
+		if (singleFileMode) {
+			destroyForcibly();
+		} else {
+			setTimeout();
+		}
+	}
+
+	/* * teardown of the file-reading * */
+
+	Thread waitingThread;
+	
+	public void destroyForcibly() {
+		tailer.stop(); // stop tailing the file
+		tailer = null;
+		t.cancel(); // release Timer thread
+		waitingThread.interrupt();
+	}
+
+	/* * timer to avoid running forever when tailing a closed file * */
+	final Timer t = new Timer();
+	TimerTask tt = null;
+	
+	private synchronized void setTimeout() {
+		cancelTimeout();
+		tt = new TimerTask() {
+			@Override public void run() {
+				System.err.println("now shutting down");
+				destroyForcibly();
+			}
+		};
+		t.schedule(tt, 500); // our timeout is 500 ms
+	}
+	
+	private void cancelTimeout() {
+		if (tt != null) {
+			tt.cancel();
+		}
+	}
+	
 }
